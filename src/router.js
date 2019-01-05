090cfef4dc50bc3b8cd7162294ce7e4cbbbe5c5c
Nathan Golba <ngolba@gmail.com>
2019-01-05 11:38:58 -0500

    Preliminary Push

--- src/router.js
+++ src/router.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Dashboard from './views/Dashboard.vue'
+import Login from './views/Login.vue'
+import BandCreator from './views/BandCreator.vue'
+import BandDashboard from './views/BandDashboard.vue'
+import EventCreator from './views/EventCreator.vue'
+import EventInfo from './views/EventInfo.vue'
 
 Vue.use(Router)
 
@@ -14,12 +20,45 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/dashboard',
+      name: 'dashboard',
+      component: Dashboard,
+      beforeEnter: (to, from, next) => {
+        if (store.state.userToken) store.dispatch('getUserPage', {userName: store.state.userName, token: store.state.userToken}).then(() => next())
+        else next('/login')
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login,
+      beforeEnter : (to, from, next) => {
+        store.state.userToken ? next('/dashboard') : next()
+      },
+    },
+    {
+      path: '/band/create',
+      name: 'bandCreator',
+      component: BandCreator
+    },
+    {
+      path: '/band/dashboard',
+      name: 'bandDashboard',
+      component: BandDashboard
+    },
+    {
+      path: '/event/create',
+      name: 'eventCreator',
+      component: EventCreator
+    },
+    {
+      path: '/event/info',
+      name: 'eventInfo',
+      component: EventInfo
+    },
+    {
+      path: '*',
+      redirect: '/'
     }
   ]
 })
