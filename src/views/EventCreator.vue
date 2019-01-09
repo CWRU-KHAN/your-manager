e9086964acb473c932b633a86cd3f3eac492a7fb
Hayley Hartman <hayleyhartman@Hayleys-MacBook-Pro.local>
2019-01-09 17:31:30 -0500

    create events

---
+++ src/views/EventCreator.vue
@@ -0,0 +1,85 @@
+<template>
+  <div>
+    <h1>Create an Event</h1>
+    <form >
+
+      <p v-if="errors.length">
+        <b>Please correct the following error(s):</b>
+        <ul>
+          <li v-for="error in errors" :key="error">{{ error }}</li>
+        </ul>
+      </p>
+
+      <!-- <img src="" alt="no image">
+      <input type="file" name="file" id="bandImg" ref="file" accept="image/*" v-on:change="processUpload()">
+      <button type="button" @click='submitImage()'>Submit Image</button>
+      <br><br>
+      <br><br> -->
+      Name of event: <input type="text" placeholder="name of event" v-model="eventName">
+      What: <input type="text" placeholder="event description" v-model="description">
+      Where: <input type="text" placeholder="location" v-model="eventlocation">
+      When: <input type="datetime-local" v-model="date">
+
+      <br><br>
+
+      <button type='button' @click='submit'>Create Event</button>
+
+    </form>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'eventCreator',
+  data() {
+    return {
+      eventName: '',
+      bandName: '',
+      description: '',
+      eventlocation: '',
+      date: '',
+      errors: [],
+      file: '',
+    }
+  },
+  methods: {
+    submit() {
+      this.errors = []
+
+        if (!this.eventName.length) this.errors.push('Event must have a name.')
+        if (!this.eventlocation.length) this.errors.push('Event must have a location')
+        if (!this.date.length) this.errors.push('Event must have a date')
+
+
+
+      if (!this.errors.length) {
+        const eventInfo = {
+            eventname: this.eventName,
+            createdAt: new Date,
+            eventlocation: this.location,
+            // eventImage: this.eventImage,
+            // bandName: this.bandName,
+            eventdescription: this.description,
+            date: this.date,
+            // ownerId: this.$store.state.userCredentials.usersid,
+            // token: this.$store.state.userCredentials.userToken,
+            // userName: this.$store.state.userCredentials.username
+        }
+
+        this.$store.dispatch('createEvent', eventInfo)
+      }
+    },
+    processUpload () {
+      this.file = this.$refs.file.files[0]
+      // this.fileChosen = true
+    }
+    // submitImage() {
+    //   let formData = new FormData()
+    //   formData.append('file', this.file)
+    // //   this.$store.dispatch('uploadImg', formData)
+    //     // .then(() => this.bandimage = this.$store.getters.getCurrentUploadedImage)
+    // }
+  }
+}
+</script>
