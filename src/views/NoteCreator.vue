176c56e882e0e7d14da76cb055b6bc1988be4690
tall karol <karolzbuczek@gmail.com>
2019-01-17 20:04:59 -0500

    added-missing-form-tags-created-note-creator

--- src/views/NoteCreator.vue
+++ src/views/NoteCreator.vue
@@ -1,28 +1,93 @@
 <template>
-  <div>
-    <h1>Write a Note</h1>
-    <form >
+  <div class="container py-4 my-3 mx-auto">
+        <form>
+    <div class="row justify-content-start">
+      <div class="col">
+        <img class="ym-luggage" src="@/assets/svg/ym-icon-luggage.svg">
+      </div>
 
-      <p v-if="errors.length">
-        <b>Please correct the following error(s):</b>
-        <ul>
-          <li v-for="error in errors" :key="error">{{ error }}</li>
-        </ul>
-      </p>
-      Subject: <input type="text" placeholder="practice monday" v-model="noteTitle">
-      <br>
-      Message: <input type="text" placeholder="can't make practice tomrrow, what about monday?" v-model="noteBody">
-      <br>
-      Regarding event on: <input type="datetime-local" v-model="calendarDate">
-      <br>
-      Band: <select name="bands" v-model="bandName">
-                <option v-for="band in bandsList" :key="band.id">{{ band.name }}</option>
-            </select>
-    
-      <br><br>
-
-      <button type='button' @click='submit'>Write Note</button>
-
+      <div class="col-12 col-md-8 col-lg-6 col-xl-5 text-left">
+        <div class="form-box">
+          <div class="row">
+            <div class="col">
+              <h1>Create A Note</h1>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col mt-4">
+                <label for="subject"> Subject
+                  <input
+                      id="subject"
+                      class="form-control"
+                      type="text"
+                      placeholder=""
+                      v-model="noteTitle">
+                </label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col mt-4">
+                <label for="message"> Message
+                  <input
+                      id="message"
+                      class="form-control"
+                      type="text"
+                      placeholder=""
+                      v-model="noteBody">
+                </label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col mt-4">
+                <label for="note-date"> Regarding Date:
+                  <input
+                      id="note-date"
+                      class="form-control"
+                      type="text"
+                      placeholder=""
+                      v-model="calendarDate">
+                </label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col mt-4">
+                <label for="band-note-link"> Band:
+                  <select
+                      id="band-note-link"
+                      name="bands"
+                      class="form-control"
+                      v-model="bandName">
+                  <option v-for="band in bandsList" :key="band.id">{{ band.name }}</option>
+                  </select>
+                </label>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col mt-4">
+                <button class="btn btn-note-create-2"
+                        @click='submit' 
+                        type="button">
+                    Write Note
+                </button>
+      <router-link class="btn btn-register" to="/dashboard">
+          <i class="fa fa-home btn-icon" aria-hidden="true"></i>
+          Back to Dashboard
+        </router-link>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col mt-4">
+                <p v-if="errors.length">
+                <b>Please correct the following error(s):</b>
+                <ul>
+                  <li v-for="error in errors" :key="error">{{ error }}</li>
+                </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
     </form>
   </div>
 </template>
@@ -91,3 +156,19 @@ export default {
   }
 }
 </script>
+
+<style>
+
+  .btn-note-create-2 {
+    background-color: #979797;
+    border: solid 1px #979797;
+    color: #ededed;
+  }
+
+  .btn-note-create-2:hover {
+    background-color: transparent;
+    border: solid 2px #979797;
+    color: #ededed
+  }
+
+</style>
