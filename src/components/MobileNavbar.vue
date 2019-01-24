37f62d077dcb8df3a4a6ec845ef03015df5109c9
natebecker11 <natebecker11@gmail.com>
2019-01-23 21:23:33 -0500

    fixed some bugs, add mobile navbar

--- src/components/MobileNavbar.vue
+++ src/components/MobileNavbar.vue
@@ -1,5 +1,8 @@
 <template>
-  
+  <nav>
+    <img src='@/assets/svg/ym-icon.svg' @click="toDashboard" alt="Your Manager Logo" :style="{height: '95%', width: '95%'}">
+<button class='inline-p' @click="logout">Logout</button>
+  </nav>
 </template>
 
 
@@ -19,11 +22,44 @@ export default {
       })
       this.$router.push({ 'path': '/' })
     },
+    toDashboard() {
+      this.$router.push({'name' : 'dashboard'})
+    }
   }
 }
 </script>
 
 
 <style scoped>
+  nav {
+    width: 100%;
+    height: 10%;
+    background-color: #373737;    
+    position: relative;
+  }
+
+  .button {
+    font-family: 'Open Sans';
+    font-size: .75em;
+    text-transform: uppercase;
+    letter-spacing: .1em;
+    padding: .5em 2em .5em 2em;
+}
+
+.button-red {
+    background-color: #d9534f;
+    border: solid 2px transparent;
+    color: #ededed;
+    margin-top: 1em;
+    border-radius: .4em;
+  }
+
+  .inline-p {
+    right: 5px;
+    top: 5px;
+    position: absolute;
+  }
+
+
 
 </style>
